cb3234fbb9b3c0c59311fb7bcf8ae962039a3e4f
miaoxing <miaoxing@bytedance.com>
2022-04-08 09:10:41 +0800

    feat: currying && instanceof

---
+++ currying.js
@@ -0,0 +1,29 @@
+function currying(fn, ...args) {
+  const length = fn.length
+
+  let allArgs = [...args]
+
+  const res = (...newArgs) => {
+    allArgs = [...allArgs, ...newArgs]
+
+    if (allArgs.length === length) {
+      return fn(...allArgs)
+    } else {
+      return res
+    }
+  }
+
+  return res
+}
+
+const add = (a,b,c) => {
+  return a + b + c
+}
+
+const a = currying(add, 1)
+
+const b = a(3)
+
+c = b(4)
+
+console.log(c)
